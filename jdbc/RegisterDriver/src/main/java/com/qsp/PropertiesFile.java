059781bd291f61a125bfbd75f6b202a4c63eb1b7
Sandesh7888 <sandeshkarad7472@gmail.com>
2025-10-12 21:44:27 +0530

    load driver

--- jdbc/RegisterDriver/src/main/java/com/qsp/PropertiesFile.java
+++ jdbc/RegisterDriver/src/main/java/com/qsp/PropertiesFile.java
@@ -7,33 +7,32 @@ import java.util.Properties;
 
 public class PropertiesFile {
 
-	public static void main(String[] args) throws IOException {
-		
-		
-		try {
-			FileInputStream file=new FileInputStream("db.properties");
-			Properties pro=new Properties();
-			pro.load(file);
-			
-			String user=pro.getProperty("user");
-			System.out.println(user);
-			
-			String password=pro.getProperty("password");
-			System.out.println(user);
-			
-			String URL=pro.getProperty("URL");
-			System.out.println(user);
-			
-			String driverClassPath=pro.getProperty("driverClassPath");
-			System.out.println(driverClassPath);
-			
-		} catch (FileNotFoundException e) {
-			
-			e.printStackTrace();
-		}
-		
-		
+    public static void main(String[] args) {
+        try {
+            // Load the properties file
+            FileInputStream file = new FileInputStream("db.properties");
+            Properties pro = new Properties();
+            pro.load(file);
 
-	}
+            // Retrieve and print each property
+            String user = pro.getProperty("user");
+            System.out.println("User: " + user);
 
+            String password = pro.getProperty("password");
+            System.out.println("Password: " + password);
+
+            String URL = pro.getProperty("URL");
+            System.out.println("URL: " + URL);
+
+            String driverClassPath = pro.getProperty("driverClassPath");
+            System.out.println("Driver Class Path: " + driverClassPath);
+
+        } catch (FileNotFoundException e) {
+            System.out.println("Properties file not found.");
+            e.printStackTrace();
+        } catch (IOException e) {
+            System.out.println("Error reading properties file.");
+            e.printStackTrace();
+        }
+    }
 }
