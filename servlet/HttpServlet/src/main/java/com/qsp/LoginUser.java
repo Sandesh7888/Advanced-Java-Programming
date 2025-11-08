bdaeefcb474a6559b2db3b05b0e8baba5ac4b5a5
Sandesh7888 <sandeshkarad7472@gmail.com>
2025-11-08 22:56:10 +0530

    servlet methods

--- servlet/HttpServlet/src/main/java/com/qsp/LoginUser.java
+++ servlet/HttpServlet/src/main/java/com/qsp/LoginUser.java
@@ -32,6 +32,16 @@ public class LoginUser extends HttpServlet {
 //		
 //	}
 	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		String username=req.getParameter("uername");
+		String password =req.getParameter("password");	
+		
+		PrintWriter p= resp.getWriter();
+		
+		if(username.equals("onkar123") && password.equals("123456")) {
+				p.write("<html><body>");
 			    p.write("<h1>Login succesfully<h1>"+username);
 			    p.write("<h1><h1>"+password);
 			    p.write("</html></body>");
