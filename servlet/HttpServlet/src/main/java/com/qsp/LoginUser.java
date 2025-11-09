f466b76dcf2ba6e1d3a1c4bc30f89317dbe723e0
Sandesh7888 <sandeshkarad7472@gmail.com>
2025-11-09 15:00:56 +0530

    servlet methods

--- servlet/HttpServlet/src/main/java/com/qsp/LoginUser.java
+++ servlet/HttpServlet/src/main/java/com/qsp/LoginUser.java
@@ -40,7 +40,11 @@ public class LoginUser extends HttpServlet {
 		
 		PrintWriter p= resp.getWriter();
 		
-		
+		if(username.equals("onkar123") && password.equals("123456")) {
+				p.write("<html><body>");
+			    p.write("<h1>Login succesfully<h1>"+username);
+			    p.write("<h1><h1>"+password);
+			    p.write("</html></body>");
 		}else {
 				p.write("<html><body>");
 			    p.write("<h1>invalid username password");
